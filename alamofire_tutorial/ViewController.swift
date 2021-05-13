eb79d42ce62895563a7b0183f1cf7a125e7aa550
arai <niimaru09@gmail.com>
2021-05-13 23:53:41 +0900

    init

---
+++ alamofire_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alamofire_tutorial
+//
+//  Created by Arai Kousuke on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
